252bfc15ec7f6e1a0188285026684fa4b90c122c
perlancar (on pc-office) <perlancar@gmail.com>
2019-01-13 20:10:56 +0700

    Initial commit

---
+++ lib/App/DuUtils.pm
@@ -0,0 +1,15 @@
+package App::DuUtils;
+
+# DATE
+# VERSION
+
+1;
+# ABSTRACT: Utilities related to the Unix "du" command
+
+=head1 SYNOPSIS
+
+This distribution provides the following command-line utilities:
+
+#INSERT_EXECS_LIST
+
+=cut
